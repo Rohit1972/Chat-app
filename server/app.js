117d246a74a15311a52a2449c3ba7009aa6fa565
Rohit1972 <rohitdogra1972@gmail.com>
2024-09-11 16:34:56 +0530

    Convo and message file added

--- server/app.js
+++ server/app.js
@@ -1,92 +1,188 @@
-const express=require('express');
-const bcryptjs =require('bcryptjs');
-const jwt =require('jsonwebtoken')
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const app=express();
-
+const app = express();
 //Connect DB
-require('./db/connection');
+require("./db/connection");
 
 //Import Files
-const Users=require('./models/Users');
+const Users = require("./models/Users");
+const Conversation = require("./models/Conversation");
+const Messages = require("./models/Messages");
 
-
-const port=process.env.PORT || 8000;
-
+const port = process.env.PORT || 8000;
 
 //App use
 app.use(express.json());
-app.use(express.urlencoded ({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.get('/',(req,res)=>{
-    res.send('Welcome');
-    
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-app.post('/api/register',async(req,res,next)=>{
-    try{
-        const {fullName, email, password}=req.body;
+app.post("/api/register", async (req, res, next) => {
+  try {
+    const { fullName, email, password } = req.body;
 
-        if(!fullName|| !email|| !password){
-            res.status(400).send('Please fill all the required fields');
-        }
-        else{
-            const isAlreadyExist= await Users.findOne({email});
-            if(isAlreadyExist) {
-                res.status(400).send('User already exists');
-            }else{
-                const newUser=new Users({fullName,email});
-                bcryptjs.hash(password, 10,(err,hashedPassword)=>{
-                    newUser.set('password',hashedPassword);
-                    newUser.save();
-                    next();
-                })
-                return res.status(200).send('User registered successfully');
-            }
-        }
-    }catch (error){
-        console.log(error,'Error');
+    if (!fullName || !email || !password) {
+      res.status(400).send("Please fill all the required fields");
+    } else {
+      const isAlreadyExist = await Users.findOne({ email });
+      if (isAlreadyExist) {
+        res.status(400).send("User already exists");
+      } else {
+        const newUser = new Users({ fullName, email });
+        bcryptjs.hash(password, 10, (err, hashedPassword) => {
+          newUser.set("password", hashedPassword);
+          newUser.save();
+          next();
+        });
+        return res.status(200).send("User registered successfully");
+      }
     }
-})
+  } catch (error) {
+    console.log(error, "Error");
+  }
+});
 
-app.post('/api/login',async(req,res,next)=>{
-    try{
-        const {email,password}=req.body;
-        if(!email || !password){
-            res.status(400).send("Please fill all required fields");
-        }else{
-            const user=await Users.findOne({email});
-            if(!user){
-                res.status(400).send('User email or password is incorrect');
-            }else{
-                const validateUser =await bcryptjs.compare(password,user.password);
-                if(!validateUser){
-                    res.status(400).send('User email or password is incorrect');
-                }else{
-                    const payload={
-                        userId:user._id,
-                        email: user.email
-                    }
-                    const JWT_SECRET_KEY =process.env.JWT_SECRET_KEY || 'THIS_IS_A_JWT_SECRET_KEY';
-                    jwt.sign(payload,JWT_SECRET_KEY,{expiresIn:84600},async(err,token)=>{
-                        await Users.updateOne({_id:user._id},{
-                            $set:{token}
-                        })
-                        user.save();
-                        next();
-                    })
-                    res.status(200).json({user: {email:user.email,fullName: user.fullName},token:user.token});
-
+app.post("/api/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send("Please fill all required fields");
+    } else {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        res.status(400).send("User email or password is incorrect");
+      } else {
+        const validateUser = await bcryptjs.compare(password, user.password);
+        if (!validateUser) {
+          res.status(400).send("User email or password is incorrect");
+        } else {
+          const payload = {
+            userId: user._id,
+            email: user.email,
+          };
+          const JWT_SECRET_KEY =
+            process.env.JWT_SECRET_KEY || "THIS_IS_A_JWT_SECRET_KEY";
+          jwt.sign(
+            payload,
+            JWT_SECRET_KEY,
+            { expiresIn: 84600 },
+            async (err, token) => {
+              await Users.updateOne(
+                { _id: user._id },
+                {
+                  $set: { token },
                 }
+              );
+              user.save();
+              next();
             }
-
+          );
+          res
+            .status(200)
+            .json({
+              user: { email: user.email, fullName: user.fullName },
+              token: user.token,
+            });
         }
-    }catch (error){
-        console.log(error,'Error');
+      }
     }
+  } catch (error) {
+    console.log(error, "Error");
+  }
+});
+
+app.post("/api/conversation", async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.body;
+    const newConversation = new Conversation({
+      members: [senderId, receiverId],
+    });
+    await newConversation.save();
+    res.status(200).send("Conversation created successfully");
+  } catch (error) {
+    console.log(error, "Error");
+  }
+});
+
+app.get("/api/conversation/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    const conversation = await Conversation.find({
+      members: { $in: [userId] },
+    });
+
+    // Use Promise.all to resolve all the async find operations in parallel
+    const conversationUserData = Promise.all(
+      conversation.map(async (conversation) => {
+        const receiverId = conversation.members.find(
+          (member) => member !== userId
+        );
+        return await Users.findById(receiverId);
+      })
+    );
+
+    // Respond with the conversation and the data (receiver information)
+    res.status(200).json(await conversationUserData);
+  } catch (error) {
+    console.log(error, "Error");
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.post('/api/message',async(req,res)=>{
+  try{
+    const {conversationId, senderId,message,receiverId=''}=req.body;
+    if(!senderId || !message) return res.status(400).send('Please fill all required fields')
+    if(!conversationId && receiverId){
+      const newConversation=new Conversation({members: [senderId, receiverId]});
+      await newConversation.save();
+      const newMessage=new Messages({conversationId: newConversation._id,senderId,message});
+      await newMessage.save();
+      return res.status(200).send('Message sent successfully');
+    }else if(!conversationId && !receiverId){
+      return res.status(400).send('Please fill all required fields')
+    }
+    const newMessage =new Messages({conversationId,senderId,message});
+    await newMessage.save();
+    res.status(200).send('Message sent successfully');
+  }catch (error){  
+    console.log(error,'Error')
+  }
 })
 
-app.listen(port, ()=>{
-    console.log('Listening on port'+port);
+app.get('/api/message/:coversationId',async (req,res)=>{
+  try{
+    const conversationId=req.params.coversationId;
+    if(conversationId === 'new') return res.status(200).json([])
+    const messages=await Messages.find({conversationId});
+    const messageUserData =Promise.all(messages.map(async(message)=>{
+      const user=await Users.findById(message.senderId);
+      return{user: {email: user.email,fullName: user.fullName},message: message.message}
+    }))
+    res.status(200).json(await messageUserData);
+  }catch(error){
+   console.log(error,'Error')
+  }
 })
+
+app.get('/api/users',async(req,res)=>{
+  try{
+    const users=await Users.find();
+    const usersData =Promise.all(users.map(async (user)=>{
+      return {user: {email: user.email, fullName: user.fullName},userId:user._id}
+    }))
+    res.status(200).json(await usersData );
+  }catch (error){
+    console.log(error,'Error')
+  }
+})
+
+app.listen(port, () => {
+  console.log("Listening on port" + port);
+});
